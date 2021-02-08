33eca820af2bf34d66811f01717393658c927bd6
Carlos Enrique <caenjuji@gmail.com>
2021-02-08 01:23:01 -0600

    Create element with animation in component home

--- src/app/modules/home/components/home/home.component.ts
+++ src/app/modules/home/components/home/home.component.ts
@@ -2,11 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 import { PlacesService } from './../../../core/services/places.service';
 import { Places } from './../../../../models/places.model';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  animations: [
+    trigger('animationContent', [
+      state('initial', style({
+        opacity: 0,
+        backgroundColor: 'green',
+        transform: 'rotate3d(0,0,0,0deg)'
+      })),
+      state('final', style({
+        opacity: 1,
+        backgroundColor: 'yellow',
+        transform: 'rotate3d(5,10,20,30deg)'
+      })),
+      transition('initial => final', animate(1000)),
+      transition('final => initial', animate(500))
+    ])
+  ]
 })
 export class HomeComponent implements OnInit {
 
@@ -14,6 +31,7 @@ export class HomeComponent implements OnInit {
   center: google.maps.LatLngLiteral = {lat: 19.5379302, lng: -96.9092582};
   zoom = 14;
   display;
+  state = 'final';
 
   constructor(
     private placesService: PlacesService
@@ -26,6 +44,18 @@ export class HomeComponent implements OnInit {
       this.places = places;
     },
     err => { console.log(err); });
+  }
+
+  animate(): void{
+    this.state = (this.state === 'final') ? 'initial' : 'final';
+  }
+
+  animationStarts(e): void{
+    console.log('Animación incia', e);
+  }
+
+  animationFinal(e): void{
+    console.log('Animación termina', e);
   }
 
   moveMap(event: google.maps.MapMouseEvent): void {
