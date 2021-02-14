ba78a1990bf709d3589c912a7ff90e323796e581
Carlos Enrique <caenjuji@gmail.com>
2021-02-13 19:06:14 -0600

    Create categories component to dashboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -5,9 +5,10 @@ import { DashboardRoutingModule } from './dashboard-routing.module';
 import { IndexComponent } from './components/index/index.component';
 import { LayoutComponent } from './components/layout/layout.component';
 import { MaterialModule } from './../material/material.module';
+import { CategoriesComponent } from './components/categories/categories.component';
 
 @NgModule({
-  declarations: [IndexComponent, LayoutComponent],
+  declarations: [IndexComponent, LayoutComponent, CategoriesComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
